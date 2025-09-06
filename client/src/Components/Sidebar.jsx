481560becb90cbef2309ef386b64aa5ba28e8027
Dimpi-145 <ndimpi145@gmail.com>
2025-09-06 21:09:58 +0530

    commit

--- client/src/Components/Sidebar.jsx
+++ client/src/Components/Sidebar.jsx
@@ -1,11 +1,23 @@
 import React from 'react'
+import {useNavigate} from 'react-router-dom';
 
 const Sidebar = ({selectedUser, setSelectedUser}) => {
+  const navigate= useNavigate();
   return (
-    <div>
-      <div className='pb-s'>
+    <div className={`bg-{#8185B2}/10 h-full p-5 rounded-r-xl overflow-y-scroll text-white ${selectedUser ? "max-md:hidden" : ''}`}>
+      <div className='pb-5'>
         <div classname= 'flex justify-between items-center'>
-          
+          <img src={assets.logo} alt="logo" classsName='max-w-40'/>
+          <div classname="relative py-2 group">
+            <img src={assets.menu_icon} alt="Menu" classsName='max-h-5 cursor-pointer'/>
+            <div classname= 'absolute top-full right-0 z-20 w-32 p-5 rounded-md bg-[#282142] border border-gray-600
+             text-gray-100 hidden group-hover:block'>
+              <p onClick={()=>navigate('/profile')} classname='cursor-pointer text-sm'>Edit Profile</p>
+              <hr className="my-2 border-t border-gray-500" />
+              <p classname='cursor-pointer text-sm'>Logout</p>
+            </div>
+          </div>
+
           
 
         </div>
